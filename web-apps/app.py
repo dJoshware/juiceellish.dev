8b19d90472f355dd35e2c6a611aefda880a56895
Josh Ellis <juiceellish@gmail.com>
2023-08-25 13:35:41 -0500

    Update venv python3.10 ->  python3.11; fixed requirements.txt; config'd caching

--- web-apps/app.py
+++ web-apps/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
-
-
-app = Flask(__name__)
-# app.app_context().push()
+from flask_caching import Cache
 
 load_dotenv()
+
+config = {
+    'DEBUG': True,
+    'CACHE_TYPE': 'SimpleCache',
+    'CACHE_DEFAULT_TIMEOUT': 300
+}
+
+app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 
 @app.route('/')
--- web-apps/venv/bin/activate.csh
+++ web-apps/venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/juiceellish/Full-Stack-Development/juiceellish.dev/playlister/venv"
+setenv VIRTUAL_ENV "/Users/juiceellish/Full-Stack-Development/juiceellish.dev/web-apps/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
